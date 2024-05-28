e7ca946b7d9a89930c48a98428a7c48b528e70c2
Eduarda França <mariaeduardafrancasantos@gmail.com>
2024-05-28 20:29:03 -0300

    Banners

---
+++ src/components/Button.vue
@@ -0,0 +1,26 @@
+<template>
+   <div>
+      <button 
+         :class="classBtn"
+         type="button"
+      >
+         {{nameBtn}}
+      </button>
+   </div>
+</template>
+
+<script>
+   export default {
+      name: 'Button',
+      props: {
+         nameBtn: {
+            type: String,
+            required: true
+         },
+         classBtn: {
+            type: String,
+            required: true
+         }
+      }
+   }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,26 +3,61 @@
       <Title :shadowTitle="shadowTitle" :frontTitle="frontTitle" />
 
       <div class="m-3 p-3 rounded-xl bg-[#FFCB33] text-[#CC7429] text-center ">
-         <p class="font-bold">Discounts are waiting for you!</p>
-         <p class="text-xl font-black ">Only Today</p>
+         <p class="font-bold mt-8">Discounts are waiting for you!</p>
+         <p class="text-xl font-black mt-5">Only Today</p>
+         <p class="text-5xl font-black mt-4">20% OFF</p>
+         <p class="text-xl font-black mt-4">Dresses & Sweatshirts</p>
+         <Button :nameBtn="btnFirst.nameBtn" :classBtn="btnFirst.classBtn" />
+      </div>
+
+      <div v-for="banner in banners" :key="banner.id">
+         <div class="m-3 rounded-xl bg-cover" :class="banner.img">
+            <div class="py-20 rounded-xl text-white text-center transparentBlack">
+               <p class="text-xl font-black mt-5">{{ banner.first }}</p>
+               <p class="text-5xl font-black mt-10">{{ banner.second }}</p>
+               <Button :nameBtn="btnSecond.nameBtn" :classBtn="btnSecond.classBtn" />
+            </div>
+         </div>
       </div>
    </div>
 </template>
 
-
 <script>
    import Title from '../components/Title.vue';
+   import Button from '../components/Button.vue';
 
    export default {
       name: 'Home',
       components: {
-         Title
+         Title,
+         Button
       },
       data() {
          return {
             shadowTitle: "New",
-            frontTitle: "Women's Clothing"
+            frontTitle: "Women's Clothing",
+            btnFirst: {
+               nameBtn: "Shop Now",
+               classBtn: "bg-black text-white rounded-full py-5 px-7 mt-6 mb-8"
+            },
+            btnSecond: {
+               nameBtn: "Shop Now",
+               classBtn: "transparentBlack text-white rounded-full py-5 px-7 mt-10"
+            },
+            banners: [
+               { id: 1, first: "Holiday Atmosphere", second: "Laid-Back style", img: "bg-[url('@/assets/image/banner-one.png')]" },
+               { id: 2, first: "Premium Quality", second: "All Case Details", img: "bg-[url('@/assets/image/banner-two.png')]" }
+            ]
          }
       }
    }
 </script>
+
+<style>
+   .transparentBlack{
+      background-color: rgba(0, 0, 0, 0.5);
+   }
+   .transparentPhoto{
+      background-color: rgba(0, 0, 0, 0.2);
+   }
+</style>
